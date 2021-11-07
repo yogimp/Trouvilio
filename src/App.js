b0e017838ef37fb156de932e36091f6f22b56404
YMP-BLACRUSH <67401418+YMP-BLACRUSH@users.noreply.github.com>
2021-11-07 20:34:57 +0700

    Services, Products and SignUp Pages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import './App.css';
 import Home from './componets/pages/Home';
+import Services from './componets/pages/Services';
+import Products from './componets/pages/Products';
+import SignUp from './componets/pages/SignUp';
 import Navbar from './componets/Navbar';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
@@ -10,7 +13,9 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        
+        <Route path="/services" element={<Services />} />
+        <Route path="/products" element={<Products />} />    
+        <Route path="/sign-up" element={<SignUp />} />    
       </Routes>
     </Router>
   );
